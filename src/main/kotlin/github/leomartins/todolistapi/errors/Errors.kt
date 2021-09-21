3cb83ccf3901fada74f5f80af3f164694ba9149b
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-09-21 19:04:21 +0100

    Add feature implementation

--- src/main/kotlin/github/leomartins/todolistapi/errors/Errors.kt
+++ src/main/kotlin/github/leomartins/todolistapi/errors/Errors.kt
@@ -1,3 +1,5 @@
 package github.leomartins.todolistapi.errors
 
 const val RESOURCE_NOT_FOUND_ERROR = "The requested resource was not found."
+
+const val ILLEGAL_OPERATION_ERROR = "The requested operation cannot be executed."
