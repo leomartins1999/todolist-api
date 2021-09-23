0714cf7b32bc96b681cb36a9b2f9531a6e0bae6b
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-09-23 18:18:21 +0100

    Fix upsert todo

--- src/main/kotlin/github/leomartins/todolistapi/dto/WriteTodo.kt
+++ src/main/kotlin/github/leomartins/todolistapi/dto/WriteTodo.kt
@@ -1,7 +1,15 @@
 package github.leomartins.todolistapi.dto
 
+import github.leomartins.todolistapi.domain.Todo
+
 data class WriteTodo(
     val title: String,
     val description: String?,
     val done: Boolean
-)
+) {
+    fun toTodo() = Todo(
+        title = title,
+        description = description,
+        done = done
+    )
+}
