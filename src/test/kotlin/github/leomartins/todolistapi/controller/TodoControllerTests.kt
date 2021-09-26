ba674b081f9e683482fe75023df9ad75131cc55b
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2021-09-26 03:04:53 +0100

    Add tests for feature

--- src/test/kotlin/github/leomartins/todolistapi/controller/TodoControllerTests.kt
+++ src/test/kotlin/github/leomartins/todolistapi/controller/TodoControllerTests.kt
@@ -2,10 +2,10 @@ package github.leomartins.todolistapi.controller
 
 import github.leomartins.todolistapi.buildTodo
 import github.leomartins.todolistapi.domain.Todo
+import github.leomartins.todolistapi.domain.TodoDifficulty
 import github.leomartins.todolistapi.interactor.GetTodosInteractor
 import github.leomartins.todolistapi.interactor.SaveTodoInteractor
 import github.leomartins.todolistapi.interactor.UpdateTodoInteractor
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.reactive.server.WebTestClient
+import java.time.LocalDateTime
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoControllerTests {
@@ -67,18 +68,15 @@ class TodoControllerTests {
     @Nested
     inner class `Save todo` {
 
-        private val todo = buildTodo()
-
-        @BeforeEach
-        fun setup() {
-            whenever(saveTodoInteractor.call(any())).doReturn(todo)
-        }
-
         @Test
         fun `saves a todo`() {
+            val todo = buildTodo()
+
+            whenever(saveTodoInteractor.call(any())).doReturn(todo)
+
             webTestClient.saveTodo(title = todo.title, description = todo.description, done = todo.done)
                 .expectStatus()
-                .is2xxSuccessful
+                .isCreated
                 .expectBody()
                 .jsonPath("$.id").isNotEmpty
                 .jsonPath("$.title").isEqualTo(todo.title)
@@ -88,9 +86,13 @@ class TodoControllerTests {
 
         @Test
         fun `description and done are optional while creating a todo`() {
+            val todo = Todo(title = "Only title")
+
+            whenever(saveTodoInteractor.call(any())).doReturn(todo)
+
             webTestClient.saveTodo(title = todo.title)
                 .expectStatus()
-                .is2xxSuccessful
+                .isCreated
                 .expectBody()
                 .jsonPath("$.title").isEqualTo(todo.title)
         }
@@ -100,6 +102,30 @@ class TodoControllerTests {
             webTestClient.saveTodo()
                 .expectStatus()
                 .is4xxClientError
+        }
+
+        @Test
+        fun `dueDate and difficulty are correctly serialized`() {
+            val dueDateString = "2021-03-03T18:00:00"
+
+            val todo = buildTodo().copy(
+                dueDate = LocalDateTime.parse(dueDateString),
+                difficulty = TodoDifficulty.MEDIUM
+            )
+
+            whenever(saveTodoInteractor.call(any())).doReturn(todo)
+
+            webTestClient.saveTodo(
+                title = todo.title,
+                dueDate = dueDateString,
+                difficulty = todo.difficulty.toString()
+            )
+                .expectStatus()
+                .isCreated
+                .expectBody()
+                .jsonPath("$.title").isEqualTo(todo.title)
+                .jsonPath("$.due_date").isEqualTo(dueDateString)
+                .jsonPath("$.difficulty").isEqualTo(todo.difficulty.toString())
         }
     }
 
@@ -142,10 +168,12 @@ class TodoControllerTests {
     private fun WebTestClient.saveTodo(
         title: String? = null,
         description: String? = null,
-        done: Boolean? = null
+        done: Boolean? = null,
+        dueDate: String? = null,
+        difficulty: String? = null
     ) = post()
         .uri("/todos")
-        .bodyValue(buildWriteTodo(title, description, done))
+        .bodyValue(buildWriteTodo(title, description, done, dueDate, difficulty))
         .exchange()
 
     private fun WebTestClient.updateTodo(
@@ -161,10 +189,14 @@ class TodoControllerTests {
     private fun buildWriteTodo(
         title: String? = null,
         description: String? = null,
-        done: Boolean? = null
+        done: Boolean? = null,
+        dueDate: String? = null,
+        difficulty: String? = null
     ) = mapOf(
         "title" to title,
         "description" to description,
-        "done" to done
+        "done" to done,
+        "due_date" to dueDate,
+        "difficulty" to difficulty
     )
 }
